3bf10f28dd0440e5a54f025e08541b9ce9f0dbbd
Stephanie McNeely Torres <stephiemct@stephanies-imac.lan>
2026-04-29 11:33:38 -0400

    project files before build phase

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,23 +1,23 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
-    phoneFormatted: "(555) 779-4407",
+    name: "COLD Community NEMT",
+    email: "dispatch@coldcommunitynemt.com",
+    phoneForTel: "334-520-4179",
+    phoneFormatted: "(334) 520-4179",
     address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
+        lineOne: "Serving Phenix City & Surrounding Areas",
+        lineTwo: "Lee · Russell · Macon · Chambers & More",
+        city: "Phenix City",
+        state: "AL",
+        zip: "",
         country: "US",
-        mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
+        mapLink: "https://maps.google.com/?q=Phenix+City+Alabama",
     },
     socials: {
         facebook: "https://www.facebook.com/",
         instagram: "https://www.instagram.com/",
     },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
+    //! Make sure you include the file protocol (e.g. https://) and
+    domain: "https://www.coldcommunitynemt.com",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
-};
+}
